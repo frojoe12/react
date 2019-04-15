404cbb04f9edcf1cf305f90fa1925b7464a83102
frojoe12 <45601644+frojoe12@users.noreply.github.com>
2019-04-15 20:04:35 +0100

    Create state-example.js
    example of a clock with time function and this.state = {object} , this.setState({object})

---
+++ state-example.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react'
+
+export default class Clock extends Component {
+
+    constructor(props) {
+        super (props)
+        this.state = {date:new Date(), count:0}
+        this.counter = 0;
+    }
+
+    componentDidMount() {
+        this.timerId = setInterval( () => this.tick(), 1000 )
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerId)
+    }
+
+    tick() {
+        this.counter++;
+        console.log(this.state.count)
+        this.setState({date: new Date(), count: this.counter })
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <div>
+                    <h1>My Clock</h1>
+                    <h2>It is {this.state.date.toLocaleTimeString()} </h2>
+                    <h3>{this.state.count}</h3>
+                </div>
+            </React.Fragment>
+        )
+    }
+}
