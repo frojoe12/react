367c0f1075b820ce09b208cb74d6617277fc70e8
frojoe12 <45601644+frojoe12@users.noreply.github.com>
2020-09-17 10:25:13 +0100

    Update toggle.js

--- toggle.js
+++ toggle.js
@@ -11,11 +11,17 @@ export default class Toggle extends React.Component {
     toggle() {
         this.setState((state) => ({isToggleOn: !state.isToggleOn}))
     }
+    toggleNoBind = () => {
+        this.setState((state,props) => ({isToggleOn: !state.isToggleOn}))   
+    }
 
     render() {
         return(
             <div>
                 <button onClick={this.toggle}>{ this.state.isToggleOn ? "ON" : "OFF" }</button>
+            </div>
+            <div>
+                <button onClick = {this.toggleNoBind}>{this.state.isToggleOn ? "Toggle On (no bind)" : "Toggle Off (no bind)}</button>
             </div>
         )
     }
